608cf46e893b52cfa98ea5898acebe897fe4cabb
Lucas <lucas.sv95@hotmail.com>
2023-07-04 10:58:58 -0300

    fix: bug deploy

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,10 +6,11 @@ import GithubProvider from "next-auth/providers/github"
 
 export const authOptions = {
   // Configure one or more authentication providers
+  secret: process.env.SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
       }),
     // ...add more providers here
   ],
